abc0a1c50b7d72a2cdce871530d3383c7c60ca21
Raul Silva Sousa <sraul.tec2000@gmail.com>
2019-04-10 00:03:55 -0300

    Adicionando o socket-io

--- src/server.js
+++ src/server.js
@@ -2,17 +2,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+
 const app = express();
+app.use(cors());
+const Server = require("http").Server(app);
+const io = require("socket.io")(Server);
+
+io.on("connection", socket => {
+  socket.on("connectionRoom", box => {
+    socket.join(box);
+  });
+});
+
 mongoose.connect(
   "mongodb+srv://raul:goweek6@cluster0-cznwt.mongodb.net/goweek6?retryWrites=true",
   {
     useNewUrlParser: true
   }
 );
-app.use(cors());
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
-app.listen(3333);
+Server.listen();
